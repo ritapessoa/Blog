176c22188227af112bfb09124a3a090e4cf9883e
ritapessoa <55166091+ritapessoa@users.noreply.github.com>
2024-02-05 17:43:31 -0300

    Update contato.php

--- contato.php
+++ contato.php
@@ -1,9 +1,15 @@
+<!-- Helpers -->
 <?php
-    include_once("templates/header.php")
+  include_once("helpers/url.php");
 ?>
-
-    <h1>Página de contato</h1>
-
+<!-- Inclusão do cabeçalho -->
 <?php
-    include_once("templates/footer.php")
+  include_once("templates/header.php");
+?>
+  <main>
+    <h1>Página de contato</h1>
+  </main>
+<!-- Inclusão do rodapé -->
+<?php
+  include_once("templates/footer.php");
 ?>
